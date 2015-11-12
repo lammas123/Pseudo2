1fe5f067867d3b698f2e9808947466cbeb796124
Robert Reinapu <robert.reinapu@khk.ee>
2015-11-12 17:30:27 +0200

    #108002154

---
+++ post.php
@@ -0,0 +1,6 @@
+<?php
+if(!empty($_POST['aadress'])){
+    $myfile = fopen("aadressid.txt", "a") or die("Unable to open file!");
+    fwrite($myfile, $_POST['aadress']."\n");
+}
+header('Location: yl4.html');
